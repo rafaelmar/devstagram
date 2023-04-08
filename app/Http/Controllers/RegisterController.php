e4dbc153e994383461fdfa9551e0240f4e846cb4
rafaelmar <raffa.damaec@gmail.com>
2023-04-08 02:40:17 +0200

    Procfile and .htaccess

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -50,6 +50,6 @@ class RegisterController extends Controller
 
         // auth()->attempt($request->only('email', 'password'));
         
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', auth()->user()->username);
         }
     }
